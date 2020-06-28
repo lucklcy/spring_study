b02109be98e2de4c61a2a23302d880a506dc3b43
lucklcy <lucklcy@hotmail.com>
2020-06-28 23:38:02 +0800

    工厂模式
    2020-06-28 23:38:01

--- src/main/java/com/itheima/ui/Client.java
+++ src/main/java/com/itheima/ui/Client.java
@@ -1,11 +1,13 @@
 package com.itheima.ui;
 
+import com.itheima.factory.BeanFactory;
 import com.itheima.service.IAccountService;
 import com.itheima.service.impl.AccountServiceImpl;
 
 public class Client {
     public static void main(String[] args) {
-        IAccountService accountService = new AccountServiceImpl();
+//        IAccountService accountService = new AccountServiceImpl();
+        IAccountService accountService = (IAccountService) BeanFactory.getBean("accountService");
         accountService.saveAccount();
     }
 }
